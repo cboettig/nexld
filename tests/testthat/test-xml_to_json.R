185efd743807d8de50d8dbd796f4b490c416c800
Carl Boettiger <cboettig@gmail.com>
2017-12-11 19:11:58 -0800

    examples

--- tests/testthat/test-xml_to_json.R
+++ tests/testthat/test-xml_to_json.R
@@ -1,5 +1,17 @@
 
 context("xml to json")
 library(nexld)
+
 ex <- system.file("extdata/example.xml", package = "nexld")
 json <- xml_to_json(ex)
+
+library(jsonld)
+
+# base-R / bizzaro pipe ->.;
+json ->.;
+  jsonld_expand(.) ->.;
+  jsonld_compact(., '{"@vocab": "http://www.nexml.org/2009/"}') ->
+  out
+
+
+
