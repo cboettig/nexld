a7c9669af5743293c70863889fcc5dd48221172b
Carl Boettiger <cboettig@gmail.com>
2018-04-21 20:27:36 -0700

    failed attempts

--- inst/notebook/get_ape_trees.R
+++ inst/notebook/get_ape_trees.R
@@ -3,7 +3,15 @@ library(nexld)
 # Utilities for converting NeXML to ape::phylo object formats
 f <- system.file("examples", "comp_analysis.xml", package="RNeXML")
 
+library(phylobase)
 
+f1 <- system.file("examples", "simmap.nex", package="RNeXML")
+f2 <- system.file("examples", "sparql.newick", package="RNeXML")
+
+t1 <- readNexus(f1) #???
+t2 <- readNewick(f2)
+
+## FIXME: Parse into phylobase trees, sane format.  Let phylobase handle conversion
 
 read_nexml <- function(x, ...){
   parse_nexml(x)
@@ -29,11 +37,37 @@ dplyr::select(length, target, source=id) %>%
 dplyr::inner_join(nodes, by = c(target = "@id")) %>%
 dplyr::select(length, source, target=id)
 
-tips <- as.character(na.omit(nodes$label))
+labels <- nodes$label
+names(labels) <- nodes$id
+labels <- labels[!is.na(labels)]
+
+edge_len <- as.numeric(edge_list$length)
+names(edge_len) <- paste(edge_list$source, edge_list$target, sep="-")
+
+edges <- as.matrix(edge_list[c("source", "target")])
+names(edges) <- c("ancestor", "descendant")
+
+## No luck:
+phy <- new("phylo4",
+           edge = edges,
+           edge.length = edge_len,
+           label = labels,
+           edge.label = setNames(character(0), character(0)))
+
+## No luck with constructor method either:
+phy <- phylo4(edges,
+              edge.length = edge_len,
+              tip.label = labels)
+
+
+
+
+
+## direct to ape doesn't have any error checking, just creates a segfault...
 phy <- list(edge = unname(as.matrix(edge_list[c("source", "target")])),
-            length = edge_list$length,
+            length = as.numeric(edge_list$length),
             Nnode = dim(nodes)[[1]] - length(tips),
-            tip.label = tips
+            tip.label = as.character(na.omit(nodes$label))
             )
 class(phy) <- "phylo"
 plot(phy)
